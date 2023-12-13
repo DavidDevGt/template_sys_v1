b8eb4b3d35c1b68ab058bf35c37fa9778015cd5c
DavidDevGt <josuedavidvl18@gmail.com>
2023-12-13 00:09:47 -0600

    Add routes for user registration and login

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,3 +1,9 @@
 <?php
 $router = new AltoRouter();
+
+// *RUTAS* //
+
+// Rutas Usuario
 $router->map('GET', '/usuarios', 'TemplateSys\Controllers\UsuarioController@index');
+$router->map('POST', '/registro', 'TemplateSys\Controllers\UsuarioController@register');
+$router->map('POST', '/login', 'TemplateSys\Controllers\UsuarioController@login');
