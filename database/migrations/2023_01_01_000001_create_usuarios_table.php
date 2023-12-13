a6315f0574871d9d7d7466b9ef70ce3a84565b19
DavidDevGt <josuedavidvl18@gmail.com>
2023-12-12 23:42:59 -0600

    Add columns to usuarios table

--- database/migrations/2023_01_01_000001_create_usuarios_table.php
+++ database/migrations/2023_01_01_000001_create_usuarios_table.php
@@ -6,5 +6,9 @@ Capsule::schema()->create('usuarios', function ($table) {
     $table->string('nombre');
     $table->string('email')->unique();
     $table->string('password');
+    $table->integer('rol_id')->unsigned()->nullable();
+    $table->boolean('activo')->default(true);
+    $table->dateTime('ultimo_login')->nullable();
+    $table->foreign('rol_id')->references('id')->on('roles');
     $table->timestamps();
 });
