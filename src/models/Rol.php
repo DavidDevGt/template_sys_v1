1f82f18b487276743f5b862ebc08af894cccf25c
DavidDevGt <josuedavidvl18@gmail.com>
2023-12-12 23:38:45 -0600

    Add Rol model to src/models directory

---
+++ src/models/Rol.php
@@ -0,0 +1,11 @@
+<?php
+namespace TemplateSys\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rol extends Model {
+    protected $table = 'roles';
+    protected $fillable = ['nombre'];
+
+    // Métodos adicionales
+}
