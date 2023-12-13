26ef27401b65d7e3fc7de313f407de32786c6f08
DavidDevGt <josuedavidvl18@gmail.com>
2023-12-13 00:09:36 -0600

    Add login and registration forms

---
+++ src/views/modules/login/index.php
@@ -0,0 +1,5 @@
+<form action="ruta_para_login" method="POST">
+    <input type="email" name="email" placeholder="Email">
+    <input type="password" name="password" placeholder="Contraseña">
+    <button type="submit">Iniciar Sesión</button>
+</form>
---
+++ src/views/modules/registro/index.php
@@ -0,0 +1,6 @@
+<form action="ruta_para_registro" method="POST">
+    <input type="text" name="nombre" placeholder="Nombre">
+    <input type="email" name="email" placeholder="Email">
+    <input type="password" name="password" placeholder="Contraseña">
+    <button type="submit">Registrar</button>
+</form>
